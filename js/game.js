0c2fd2b0866ba24bc9779af42ef46d967b8b2dce
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-04-14 16:42:52 +0000

    added changes in the game.js

--- js/game.js
+++ js/game.js
@@ -6,13 +6,28 @@
 // Created on: Mar 2025
 // This file contains the JS functions for index.html
 
-
+/**
+ * Start Phaser Game.
+ */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
+// Create the Phaser game instance
 const game = new Phaser.Game(config)
 console.log(game)
