13b4995f6e4c91ba2fa1ef001386725c27ba0a0b
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-04-17 17:24:11 +0000

    inital

--- spashScene.js
+++ spashScene.js
@@ -3,30 +3,33 @@
 // Copyright (c) 2025 kyle.matthew.magnaye All rights reserved
 //
 // Created by: kyle.matthew.magnaye
-// Created on: Mar 2025
+// Created on: Apirl 17 2025
 // This is the Splash Scene
 
-/**
- * This class is the Splash Scene.
-*/
 class SpashScene extends Phaser.Scene {
-  constructor () {
-    super({ key: 'spashScene' })
-  }
-
-  init (data) {
-    this.cameras.main.setBackgroundColor('#ffffff')
-  }
-
-  preload () {
-    console.log('Spash Scene')
-  }
-
-  create (data) {
-  }
-
-  update (time, delta) {
-  }
+constructor () {
+  super({ key: 'spashScene' })
 }
 
-export default SplashScene
+init (data) {
+  this.cameras.main.setBackgroundColor('#ffffff')
+}
+
+preload () {
+  console.log('Spash Scene')
+}
+
+
+
+
+
+
+
+
+create (data) {
+}
+
+update (time, delta) {
+}
+}
+export default SpashScene
